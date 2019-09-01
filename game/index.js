27b313ac27b79e8bfa0bcef391d236ef4a3c5c28
emilytimer <by0807103@163.com>
2019-09-01 12:16:23 +0800

    generate array

--- game/index.js
+++ game/index.js
@@ -1,1 +1,13 @@
-console.log("abc")
+$(function(){
+    // generate 1 ~ 9 suffertd array
+    function generateArray(){
+        let arr = [...Array(10).keys()]
+        arr.shift();
+        arr.sort((x,y)=>{
+            return Math.random()*2 - 1
+        })
+        return arr;
+    }
+    let arr = generateArray();
+    console.log("arr:",arr)
+})
